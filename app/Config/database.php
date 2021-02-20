60fb9aa7456cb913e27013b674ba2fa89ec6a5bd
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-20 23:35:40 +0530

    update

---
+++ app/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+public $default = array (
+  'datasource' => 'Database/Mysql',
+  'host' => 'localhost',
+  'database' => 'sonerezh',
+  'login' => 'TestUser',
+  'password' => 'TestUser',
+  'prefix' => 'so_',
+  'persistent' => false,
+  'encoding' => 'utf8',
+);
+}
